c7ff8f9287a3f44437f169691ef6906df0150b6e
Brennan Heisler <learn@heislercreative.com>
2017-08-25 03:27:46 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('.target')
+}
+
+function increaseRankBy(n) {
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n
+  }
+}
+
+function deepestChild() {
+  var child = document.getElementById('grand-node')
+
+  while (child) {
+    var dChild = child
+    child = dChild.children[0]
+  }
+  return dChild
+}
